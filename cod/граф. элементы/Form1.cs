014e83be820c33e0cb3f90d1585b7fa7a6a6c784
koala1708 <konckova.alevtina@yandex,ru>
2020-06-28 19:18:36 +0300

    comchange

--- cod/граф. элементы/Form1.cs
+++ cod/граф. элементы/Form1.cs
@@ -26,18 +26,7 @@ namespace граф.элементы
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
-            g.Clear(Color.White);
-            if (listBox1.SelectedIndex == 0)
-                g.FillRectangle(Brushes.Aquamarine, Square);
-            if (listBox1.SelectedIndex == 1)
-                g.FillEllipse(Brushes.CadetBlue, Circle);
-            if (listBox1.SelectedIndex == 2)
-                g.FillRectangle(Brushes.Pink, Rectangle);
-            if (listBox1.SelectedIndex == 3)
-                g.FillEllipse(Brushes.RoyalBlue, Ellipse);
-            if (listBox1.SelectedIndex == 4)
-                g.FillPolygon(Brushes.YellowGreen,Triangle);
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
